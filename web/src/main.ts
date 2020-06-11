30b22e8bbac59826d5907eda7fea068c4e53e3ac
Juan José Torres <juanjtorres@pm.me>
2020-06-11 16:42:40 -0500

    bootstrap-vue

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue} from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+// Install BootstrapVue
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
